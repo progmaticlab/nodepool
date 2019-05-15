69d74525f6e1a2b2f9574da7cbbf742f74276da9
Andrey Pavlov <andrey-mp@yandex.ru>
2019-05-15 22:12:06 +0300

    allow to pass docker image name to build

--- build-dockerimages.sh
+++ build-dockerimages.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
+# $1 - name of component to build. all components will be built if not present
+
 NODEPOOL_DOCKERREPO=${NODEPOOL_DOCKERREPO:-"local"}
 NODEPOOL_VERSION=${NODEPOOL_VERSION:-"0.5.1"}
 
-if [[ "$NODEPOOL_CLEAN" == "1" ]]; then
-    docker image rm ${NODEPOOL_DOCKERREPO}/nodepool-launcher:${NODEPOOL_VERSION}
-    docker image rm ${NODEPOOL_DOCKERREPO}/nodepool-builder:${NODEPOOL_VERSION}
-fi
+function build() {
+  local name=$1
+  if [[ "$NODEPOOL_CLEAN" == "1" ]]; then
+    docker image rm ${NODEPOOL_DOCKERREPO}/nodepool-$name:${NODEPOOL_VERSION}
+  fi
+  docker build --target nodepool-launcher -t ${NODEPOOL_DOCKERREPO}/nodepool-$name:${NODEPOOL_VERSION} .
+  if [[ "$NODEPOOL_PUSH" == "1" ]]; then
+    docker push ${NODEPOOL_DOCKERREPO}/nodepool-$name:${NODEPOOL_VERSION}
+  fi
+}
 
-docker build --target nodepool-launcher -t ${NODEPOOL_DOCKERREPO}/nodepool-launcher:${NODEPOOL_VERSION} .
-docker build --target nodepool-builder -t ${NODEPOOL_DOCKERREPO}/nodepool-builder:${NODEPOOL_VERSION} .
-
-if [[ "$NODEPOOL_PUSH" == "1" ]]; then
-    docker push ${NODEPOOL_DOCKERREPO}/nodepool-launcher:${NODEPOOL_VERSION}
-    docker push ${NODEPOOL_DOCKERREPO}/nodepool-builder:${NODEPOOL_VERSION}
+if [[ -n "$1" ]]; then
+  build $1
+else
+  build launcher
+  build builder
 fi
